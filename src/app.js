ace149cc05b32f42b4028d5d662a08fef97e3379
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-09-21 18:19:21 -0500

    corriendo test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo!");
+});
+
+app.get("/mensaje", (req, res) => {
+  res.send("este es mi servidor usando express");
+});
+
+// app.listen(port, () => {
+//   console.log(`La aplicación está escuchando en http://localhost:${port}`);
+// });
+
 module.exports = app;
+
